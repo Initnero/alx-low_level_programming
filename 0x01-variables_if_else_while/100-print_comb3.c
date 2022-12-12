15d5a403dd9c8fcd354854bc756c5e3c46c2b663
Initnero <initnero@gmail.com>
2022-12-12 02:26:28 -0800

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Description:  prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
