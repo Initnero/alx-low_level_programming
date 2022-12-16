88f8dc36445a943ef389143b13945a4ef3a5d28e
Initnero <initnero@gmail.com>
2022-12-16 07:48:32 -0800

    Write a function that prints a triangle, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ */
+void print_square(int size)
+{
+	int count1, count2;
+
+	count1 = 0;
+
+	if (size > 0)
+	{
+		while (count1 < size)
+		{
+			count2 = 0;
+			while (count2 < size)
+			{
+				_putchar('#');
+				count2++;
+			}
+			_putchar('\n');
+			count1++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
